a6d64eff7fa993cdcb1c51330708913f08890af7
shonmikl <mikhailegorov676@gmail.com>
2021-11-04 21:29:03 +0300

    Initial commit

---
+++ src/JavaSweeper.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+
+public class JavaSweeper extends JFrame
+{
+    public static void main(String[] args)
+    {
+           new JavaSweeper().setVisible(true);
+    }
+
+    public JavaSweeper ()
+    {
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+}
