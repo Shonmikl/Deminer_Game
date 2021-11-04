70870a8bf6a84ce9a6c9e08a9d8d9bdeab7be6f0
shonmikl <mikhailegorov676@gmail.com>
2021-11-04 21:49:18 +0300

    added methods

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -1,15 +1,30 @@
 import javax.swing.*;
+import java.awt.*;
 
-public class JavaSweeper extends JFrame
-{
-    public static void main(String[] args)
-    {
-           new JavaSweeper().setVisible(true);
+public class JavaSweeper extends JFrame {
+    private JPanel panel;
+
+    public static void main(String[] args) {
+        new JavaSweeper();
     }
 
-    public JavaSweeper ()
-    {
+    private JavaSweeper() {
+        initPanel();
+        initFrame();
+    }
+
+    private void initPanel() {
+        panel = new JPanel();
+        panel.setPreferredSize(new Dimension(500, 300));
+        add(panel);
+    }
+
+    private void initFrame() {
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        setTitle("Java Sweeper");
+        setVisible(true);
+        setResizable(false);
+        pack();
+        setLocationRelativeTo(null);
     }
-
 }
