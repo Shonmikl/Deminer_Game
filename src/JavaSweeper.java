3f4feb7896307f1c6bb86b2c43bee5816ac5435a
shonmikl <mikhailegorov676@gmail.com>
2021-11-05 19:38:35 +0300

    added jar

--- src/JavaSweeper.java
+++ src/JavaSweeper.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.util.Locale;
 
 import sweeper.Box;
+import sweeper.Coord;
 
 public class JavaSweeper extends JFrame {
     private JPanel panel;
@@ -28,7 +29,8 @@ public class JavaSweeper extends JFrame {
                 super.paintComponent(g);
 
                 for (Box box: Box.values()) {
-                    g.drawImage((Image) box.image, box.ordinal() * IMAGE_SIZE, 0, this);
+                    Coord coord = new Coord(box.ordinal(), 0);
+                    g.drawImage((Image) box.image, coord.x * IMAGE_SIZE, coord.y * IMAGE_SIZE, this);
                 }
             }
         };
@@ -54,6 +56,7 @@ public class JavaSweeper extends JFrame {
     private Image getImage (String name) {
         String fileName = "source/img/" + name + ".png";
         ImageIcon icon = new ImageIcon(fileName);
+        // required  ImageIcon icon = new ImageIcon(getClass().getResource(fileName)); ---> but doesn't work //
         return icon.getImage();
     }
 }
